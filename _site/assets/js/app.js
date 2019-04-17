1eb1c3305581d622201cb65556df0a3bf062337e
Daniel Kalivoda <danek.kalivoda@gmail.com>
2019-04-17 21:10:56 +0200

    styling

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    console.log( "ready!" );
-});
+function toggleClass(el, className){
+    el.classList.toggle(className);
+}
+
+window.onload = function(){ 
+    
+    body = document.getElementsByTagName('body')[0];
+    menuToggle = document.getElementById('menu-toggle');
+    menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function(event) {
+        toggleClass(menu, 'hidden');
+        toggleClass(menuToggle, 'opened');
+        event.stopPropagation();
+    });
+    body.addEventListener('click', function(event){
+        if (menuToggle.classList.contains("opened")) {
+            menu.classList.add("hidden")
+            menuToggle.classList.remove("opened")
+        }
+    })
+
+}
