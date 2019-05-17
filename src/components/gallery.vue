5951f57ff30c64bfb1fb81ec562c1014f607a75c
Daniel Kalivoda <danek.kalivoda@gmail.com>
2019-05-17 22:08:23 +0200

    styling gallery

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
+  <div class="flex flex-wrap w-full">
     <gallery :images="images" :index="index" @close="index = null"></gallery>
-    <div class="inline-flex w-1/5"
+    <div class="inline-flex w-1/5 group cursor-pointer"
       v-for="(image, imageIndex) in images"
       :key="imageIndex"
       @click="index = imageIndex">
-        <div class="block relative overflow-hidden aspect-ratio-16/9 transition group-hover:scale-110 w-full">
+        <div class="block relative overflow-hidden aspect-ratio-square transition scale-90 group-hover:scale-100 w-full">
           <img :src="image" class="w-full h-full absolute top-0 left-0 object-cover z-10 transition-slow opacity-0 opacity-100">
+          <div class="absolute w-full h-full -translate-y-1/2 z-0 transition-slow">
+            <div class="spinner"></div>
+          </div>
         </div>
     </div>
   </div>
